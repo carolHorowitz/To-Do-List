7a8c28bcb5722569ba8f709c84265c469496c653
carolHorowitz <carolvet.horowitz@gmail.com>
2022-11-22 15:42:48 -0300

    Código js refeito

---
+++ formulário.js
@@ -0,0 +1,12 @@
+buttonAddEventListener("click", (event) => {
+    EventTarget.preventDefault();
+})
+
+const input = document.querySelector(tarefa).value;
+
+function novaTarefa(input) {
+    if (input.value === "") {
+} else {
+    
+}
+}
