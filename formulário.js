a6d40c673cdb8b3b3ad413343e3b28c2420c6f54
carolHorowitz <carolvet.horowitz@gmail.com>
2022-11-23 12:27:10 -0300

    JavaScript final

--- formulário.js
+++ formulário.js
@@ -1,18 +1,29 @@
 
 
-const button = document.getElementById("btn");
+function novaTarefa() {
+    const lista = document.getElementById("minhaLista");
+    const tarefa = document.getElementById("tarefa").value;
+    const span=document.createElement("span")
+    span.append(tarefa);
+    console.log(span)
 
+    const item = document.createElement("li");
+    const excluir = document.createElement("button")
+    const X=document.createTextNode("X")
+    excluir.appendChild(X);
 
-button.addEventListener("click", (event) => {
-    EventTarget.preventDefault();
-})
+    item.append(span);
+    item.append(excluir);
+    
+    lista.append(item);
 
-const tarefa = document.getElementById("tarefa").value;
-
-
-function novaTarefa(tarefa) {
-    if (tarefa.value === "") {
-} else {
+    span.onclick = function() {
+        
+        span.classList.add("checked")
+    }
+    excluir.onclick=function() {
+        item.remove()
+    }
     
 }
-}
+
