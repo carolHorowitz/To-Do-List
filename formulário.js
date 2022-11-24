0cd9e81c67b430e83dab62fa1951f5e8a68e13eb
carolHorowitz <carolvet.horowitz@gmail.com>
2022-11-24 15:49:03 -0300

    Adicionando css na lista de tarefas adicionadas e melhorando funções no javascript

--- formulário.js
+++ formulário.js
@@ -1,28 +1,42 @@
 
 
 function novaTarefa() {
+
+    const tarefa=document.getElementById("tarefa");
+    
+    if (tarefa.value =="") {
+        alert("Digite uma tarefa!")
+        return false
+    }
+
     const lista=document.getElementById("minhaLista");
-    const tarefa=document.getElementById("tarefa").value;
+
     const span=document.createElement("span")
-    span.append(tarefa);
-    console.log(span)
+    span.append(tarefa.value);
+    
 
     const item=document.createElement("li");
+
     const excluir=document.createElement("button")
     const X=document.createTextNode("X")
     excluir.appendChild(X);
 
     item.append(span);
+
     item.append(excluir);
     
     lista.append(item);
 
+    tarefa.value="";
+   
     span.onclick = function() {
-        
         span.classList.add("checked")
     }
+
+    excluir.classList.add("excluir-itens")
+
     excluir.onclick=function() {
-        item.remove()
+       item.remove()
     }
     
 }
